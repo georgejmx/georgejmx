50893dc8676050a9453219a167b878053cc38cd8
gm <georgejmx@pm.me>
2022-09-28 11:41:47 +0100

    improved profile image and fixed blog reference

--- frontend/main.js
+++ frontend/main.js
@@ -74,7 +74,7 @@ document.getElementById("home-btn").addEventListener("click", () => {
 document.getElementById("projects-btn").addEventListener("click", () => {
   changeTabSelection(2);
 });
-document.getElementById("blog-btn").addEventListener("click", () => {
+document.getElementById("story-btn").addEventListener("click", () => {
   changeTabSelection(3);
 });
 
@@ -82,22 +82,22 @@ document.getElementById("blog-btn").addEventListener("click", () => {
 function changeTabSelection(val) {
   const home = document.getElementById("home-box");
   const projects = document.getElementById("projects-box");
-  const blog = document.getElementById("blog-box");
+  const stories = document.getElementById("story-box");
   switch (val) {
     case 1:
       home.style.display = "initial";
       projects.style.display = "none";
-      blog.style.display = "none";
+      stories.style.display = "none";
       break;
     case 2:
       home.style.display = "none";
       projects.style.display = "initial";
-      blog.style.display = "none";
+      stories.style.display = "none";
       break;
     case 3:
       home.style.display = "none";
       projects.style.display = "none";
-      blog.style.display = "initial";
+      stories.style.display = "initial";
       break;
   }
 }
