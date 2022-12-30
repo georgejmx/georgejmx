e10afeb4b176ab0f2d689a470dd64fae218a6f4f
george <tylerdurden@tylers-MacBook-Air.local>
2022-12-30 22:00:59 +0000

    chore: setting up admin page

---
+++ frontend/src/admin.ts
@@ -0,0 +1,9 @@
+import "virtual:windi.css";
+
+const inputBox: HTMLDivElement = document.getElementById(
+  "inputs-box"
+) as HTMLDivElement;
+inputBox.innerHTML =
+  "<p>Here is where the dynamically generated inputs go :)</p>";
+
+export {};
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,4 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import WindiCSS from "vite-plugin-windicss";
 
@@ -5,6 +6,10 @@ export default defineConfig({
   plugins: [WindiCSS()],
   build: {
     rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        admin: resolve(__dirname, "admin.html"),
+      },
       output: {
         entryFileNames: "index.ashes.js",
         assetFileNames: "index.ashes.css",
