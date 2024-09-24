c9a800d30e894c088e4b14b7820347db5eca58de
georgejmx <georgejm2358@protonmail.com>
2024-09-24 18:51:34 +0100

    Rework to simple static site (#36)
    * chore: rework to simple static site
    
    * fix: css
    
    * build: github pages
    
    * fix: ci and projects styles
    
    * fix: z-index
    
    * remove codeql

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "rollup";
 import typescript from "@rollup/plugin-typescript";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
 
 export default defineConfig([
     {
@@ -19,29 +18,5 @@ export default defineConfig([
             format: "iife",
         },
         plugins: [typescript(), nodeResolve()],
-    },
-    {
-        input: "src/client/admin.ts",
-        output: {
-            file: "assets/bundles/admin.js",
-            format: "iife",
-            name: "admin",
-        },
-        plugins: [
-            typescript(),
-            nodeResolve({ preferBuiltins: true }),
-            commonjs({
-                include: /node_modules/,
-                requireReturnsDefault: "auto",
-            }),
-        ],
-    },
-    {
-        input: "src/client/story.ts",
-        output: {
-            file: "assets/bundles/story.js",
-            format: "iife",
-        },
-        plugins: [typescript()],
     },
 ]);
--- src/client/index.ts
+++ src/client/index.ts
@@ -1,15 +1,25 @@
-import { changeTabSelection } from "./shared.js";
+// Updates scrolling content upon menu button selection
+function changeTabSelection(val: number) {
+    const home = document.getElementById("home-box") as HTMLDivElement;
+    const projects = document.getElementById("projects-box") as HTMLDivElement;
+    switch (val) {
+        case 1:
+            home.style.display = "initial";
+            projects.style.display = "none";
+            break;
+        case 2:
+            home.style.display = "none";
+            projects.style.display = "initial";
+            break;
+    }
+}
 
 const homeBtn = document.getElementById("home-btn") as HTMLButtonElement;
 const projectBtn = document.getElementById("projects-btn") as HTMLButtonElement;
-const storyBtn = document.getElementById("story-btn") as HTMLButtonElement;
 
 homeBtn.addEventListener("click", () => {
     changeTabSelection(1);
 });
 projectBtn.addEventListener("click", () => {
     changeTabSelection(2);
-});
-storyBtn.addEventListener("click", () => {
-    changeTabSelection(3);
 });
